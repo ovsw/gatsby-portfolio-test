29faae4ccb09a1698d8015c6b78747c004866af5
Ovi Savescu <ovi@ovswebsites.com>
2020-02-11 20:21:29 +0200

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+// Load variables from `.env` as soon as possible
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV || 'development'}`
+})
+
+const clientConfig = require('./client-config')
+const token = process.env.SANITY_READ_TOKEN
+
+
+
+module.exports = {
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        ...clientConfig.sanity,
+        token: process.env.SANITY_READ_TOKEN,
+        watchMode: true,
+        overlayDrafts: true
+      }
+    }
+  ]
+}
